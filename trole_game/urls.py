9e1877a82cd75eb7e10b8d9ccfcbcdf40bfb9042
Kate Makarova <kate.makarova@hotmail.com>
2024-12-25 15:12:18 -0500

    characters

--- trole_game/urls.py
+++ trole_game/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, UserHome, UserGetByUsername
+from .views import index, UserHome, UserGetByUsername, GetGameById, GetEpisodeList, GetCharacterList
 
 urlpatterns = [
     path('api/', index, name='index'),
     path('api/home', UserHome.as_view(), name='home'),
-    path('api/user/get-by-username/<str:username>', UserGetByUsername.as_view())
+    path('api/user/get-by-username/<str:username>', UserGetByUsername.as_view()),
+    path('api/game/<int:id>', GetGameById.as_view(), name='game'),
+    path('api/episode-list/<int:game_id>', GetEpisodeList.as_view(), name='episode_list'),
+    path('api/character-list/<int:game_id>', GetCharacterList.as_view(), name='character_list')
 ]
--- trole_game/views.py
+++ trole_game/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from trole_game.models import Character, Game, UserGameParticipation
+from trole_game.models import Character, Game, UserGameParticipation, Episode
+
 
 def index(request):
     return JsonResponse({
@@ -17,7 +18,7 @@ class UserHome(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        user_id = 1
+        user_id = request.user.id
         data = []
         participations = UserGameParticipation.objects.filter(user_id=user_id)
         for participation in participations:
@@ -71,4 +72,82 @@ class UserGetByUsername(APIView):
             })
         return Response({"data": data})
 
+class GetGameById(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
+        game = Game.objects.get(pk=id)
+        data = {
+            "id": game.id,
+            "name": game.name,
+            "image": game.image,
+            "total_characters": game.total_characters,
+            "total_posts": game.total_posts,
+            "total_users": game.total_users,
+            "total_episodes": game.total_episodes,
+            "rating": game.rating.name,
+            "description": game.description
+        }
+        return Response({"data": data})
+
+class GetEpisodeList(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, game_id):
+        episodes = Episode.objects.filter(game_id=game_id)
+        data = []
+
+        for episode in episodes:
+            is_mine = False
+            for character in episode.characters.all():
+                if character.user.id == request.user.id:
+                    is_mine = True
+                    break
+            if episode.category == None:
+                category = ''
+            else:
+                category = episode.category.name
+
+            if episode.last_post_author == None:
+                last_post_author = None
+            else:
+                last_post_author = {
+                    "id": episode.last_post_author.id,
+                    "name": episode.last_post_author.name
+                },
+            data.append({
+                "id": episode.id,
+                "name": episode.name,
+                "image": episode.image,
+                "category": category,
+                "status": episode.status.name,
+                "last_post_date": episode.last_post_date,
+                "last_post_autor": last_post_author,
+                "description": episode.description,
+                "is_mine": is_mine
+            })
+        return Response({"data": data})
+
+class GetCharacterList(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, game_id):
+        characters = Character.objects.filter(game_id=game_id)
+        data = []
+
+        for character in characters:
+            data.append({
+                "id": character.id,
+                "name": character.name,
+                "avatar": character.avatar,
+                "user": {
+                    "id": character.user.id,
+                    "name": character.user.username
+                },
+                "is_mine": (character.user.id == request.user.id)
+            })
+        return Response({"data": data})
 

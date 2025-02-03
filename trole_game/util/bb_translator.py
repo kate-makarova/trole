277bced296fb96f234e651039295636ea2f0f728
Kate Makarova <kate@donkeymob.com>
2025-02-03 14:25:05 -0500

    bb codes

--- trole_game/util/bb_translator.py
+++ trole_game/util/bb_translator.py
@@ -1,18 +1,42 @@
 import re
 
-def translate_bb(bb_code):
-    result = bb_code.replace('\n', '</p><p>')
-    result = '<p>'+result+'</p>'
-    result = result.replace('[b]', '<b>').replace('[/b]', '</b>')
-    result = result.replace('[i]', '<i>').replace('[/i]', '</i>')
-    result = result.replace('[u]', '<u>').replace('[/u]', '</u>')
-    result = result.replace('[s]', '<s>').replace('[/s]', '</s>')
-    result = result.replace('[code]', '<code>').replace('[/code]', '</code>')
-    result = result.replace('[quote]', '<blockquote>').replace('[/quote]', '</blockquote>')
-    result = re.sub(r'\[url=([^]]*)\]([^[]*)\[\/url\]', r'<a href="\1">\2</a>', result)
-    result = result.replace('[img]', '<img src="').replace('[/img]', '" />')
-    result = re.sub(r'\[color=([^]]*)\]([^[]*)\[\/color\]', r'<span style="color: \1">\2</span>', result)
-    result = re.sub(r'\[size=([^]]*)\]([^[]*)\[\/size\]', r'<span style="font-size: \1">\2</span>', result)
-    result = re.sub(r'\[font=([^]]*)\]([^[]*)\[\/font\]', r'<span style="font-family: \1">\2</span>', result)
 
-    return result
+def translate_bb(text):
+    patterns = [
+        (r"\[url=(.*?)\](.*?)\[\/url\]", '<a href="{find1}">{find2}</a>', 2),
+        (r"\[font=(.*?)\](.*?)\[\/font\]", '<span style="font-family:{find1}">{find2}</span>', 2),
+        (r"\[color=(.*?)\](.*?)\[\/color\]", '<span style="color:{find1}">{find2}</span>', 2),
+        (r"\[size=(.*?)\](.*?)\[\/size\]", '<span style="fond-size:{find1}em">{find2}</span>', 2),
+        (r"\[b\](.*?)\[\/b\]", '<b>{find1}</b>', 1),
+        (r"\[i\](.*?)\[\/i\]", '<i>{find1}</i>', 1),
+        (r"\[u\](.*?)\[\/u\]", '<u>{find1}</u>', 1),
+        (r"\[s\](.*?)\[\/s\]", '<s>{find1}</s>', 1),
+        (r"\[img\](.*?)\[\/img\]", '<img src="{find1}" />', 1),
+    ]
+
+    for pattern in patterns:
+        matches = re.finditer(pattern[0], text)
+
+        if pattern[2] == 2:
+            for match in matches:
+                match_start = match.start(0)
+                match_end = match.end(0)
+                find1 = match.group(1)
+                find2 = match.group(2)
+
+                replacement = pattern[1].replace('{find1}', find1)
+                replacement = replacement.replace('{find2}', translate_bb(find2))
+                text = text[:match_start] + replacement + text[match_end:]
+
+        if pattern[2] == 1:
+            for match in matches:
+                match_start = match.start(0)
+                match_end = match.end(0)
+                find1 = match.group(1)
+
+                replacement = pattern[1].replace('{find1}', translate_bb(find1))
+                text = text[:match_start] + replacement + text[match_end:]
+
+    return text
+
+

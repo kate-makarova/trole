0d6ee90bf2cf7e89f742321b3e177d9eca58e4e9
Kate Makarova <kate@donkeymob.com>
2025-02-04 10:22:50 -0500

    data

--- trole_game/util/bb_translator.py
+++ trole_game/util/bb_translator.py
@@ -5,8 +5,8 @@ def translate_bb(text):
     patterns = [
         (r"\[url=(.*?)\](.*?)\[\/url\]", '<a href="{find1}">{find2}</a>', 2),
         (r"\[font=(.*?)\](.*?)\[\/font\]", '<span style="font-family:{find1}">{find2}</span>', 2),
-        (r"\[color=(.*?)\](.*?)\[\/color\]", '<span data-replace="color:{find1}">{find2}</span>', 2),
-        (r"\[size=(.*?)\](.*?)\[\/size\]", '<span data-replace="font-size:{find1}">{find2}</span>', 2),
+        (r"\[color=(.*?)\](.*?)\[\/color\]", '<span style="color:{find1}">{find2}</span>', 2),
+        (r"\[size=(.*?)\](.*?)\[\/size\]", '<span style="font-size:{find1}">{find2}</span>', 2),
         (r"\[b\](.*?)\[\/b\]", '<b>{find1}</b>', 1),
         (r"\[i\](.*?)\[\/i\]", '<i>{find1}</i>', 1),
         (r"\[u\](.*?)\[\/u\]", '<u>{find1}</u>', 1),

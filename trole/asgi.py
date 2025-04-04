66973a9293d3ab7f8833a428d48608a064e553d0
Kate Makarova <kate@donkeymob.com>
2025-04-04 11:54:53 -0400

    logging setting

--- trole/asgi.py
+++ trole/asgi.py
@@ -13,6 +13,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trole.settings')
 django_asgi_app = get_asgi_application()
 from messanger.authentication import JWTAuthMiddleware
 from messanger.routing import websocket_urlpatterns

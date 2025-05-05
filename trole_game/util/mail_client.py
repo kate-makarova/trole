62ce38f9b5d6a85c15fa57f6cb8919479043943f
Kate Makarova <kate@donkeymob.com>
2025-05-05 12:24:02 -0400

    maigun

--- trole_game/util/mail_client.py
+++ trole_game/util/mail_client.py
@@ -1,42 +1,20 @@
-import boto3
-from botocore.exceptions import ClientError
+import requests
+from django.conf import settings
+
 
 class MailClient:
     def __init__(self):
-        self.sender = "Trole Online System <system@trole.online>"
-        self.aws_region = "us-east-1"
-        self.charset = "UTF-8"
+        self.sender = "Trole Online <system@mail.trole.online>"
+        self.url = "https://api.mailgun.net/v3/mail.trole.online/messages"
+        self.key = "UTF-8"
     
     def send(self, subject, body_text, body_html, recipient):
-        client = boto3.client('ses', region_name=self.aws_region)
-
-        try:
-            response = client.send_email(
-                Destination={
-                    'ToAddresses': [
-                        recipient,
-                    ],
-                },
-                Message={
-                    'Body': {
-                        'Html': {
-                            'Charset': self.charset,
-                            'Data': body_html,
-                        },
-                        'Text': {
-                            'Charset': self.charset,
-                            'Data': body_text,
-                        },
-                    },
-                    'Subject': {
-                        'Charset': self.charset,
-                        'Data': subject,
-                    },
-                },
-                Source=self.sender,
-            )
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-            return False
-        else:
-            return True
+        requests.post(
+            self.url,
+            auth=("api", settings.MAILGUN_API_KEY),
+            data={"from": self.sender,
+                  "to": ""+recipient+">",
+                  "subject": subject,
+                  "text": body_text,
+                  "html": body_html
+                  })

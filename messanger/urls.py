4b3182f82c08d81f292f016f5554e28b59ba6eb1
Kate Makarova <kate@donkeymob.com>
2025-05-07 17:22:14 -0400

    user autocomplete

--- messanger/urls.py
+++ messanger/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("api/private-chat-messages/<int:id>", views.PrivateChatGetMessages.as_view(), name="private_chat_get_messages"),
     path("api/private-chat-messages/<int:id>/<int:page>", views.PrivateChatGetMessages.as_view(),
          name="private_chat_get_messages_full"),
-    path("api/private-chat-add", views.AddPrivateChat.as_view(), name="add_private_chat"),
-    path("api/private-chat-add-participant", views.PrivateChatAddParticipant.as_view(), name="add_private_chat_participant"),
+    path("api/private-chat/create", views.AddPrivateChat.as_view(), name="add_private_chat"),
+    path("api/private-chat/add-participant", views.PrivateChatAddParticipant.as_view(), name="add_private_chat_participant"),
     path("api/last-open-chat", views.LastOpenChat.as_view(), name="last_open_chat"),
     path("api/last-open-chat/update", views.LastOpenChatUpdate.as_view(), name="last_open_chat_update"),
 ]
